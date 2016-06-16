6ac8fb05b5bac79ae1a1d8401383cecffa85c4ed
Rob Coffman <robcoffman@github.com>
2016-06-16 15:46:58 -0700

    adding drush scripts

---
+++ config/drush/aliases.drushrc.php
@@ -0,0 +1,10 @@
+<?php
+$aliases['fga'] = array (
+  'root' => '/home/homepage.la-z-boyga.com/public_html',
+  'uri' => 'http://homepage.la-z-boyga.com',
+);
+
+$aliases['ebco'] = array (
+  'root' => '/home/homepage/public_html',
+  'uri' => 'http://homepage.la-z-boyaz.com',
+);
