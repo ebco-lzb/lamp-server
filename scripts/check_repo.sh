03d1ff2f71f60f2b5a5b674bf1adcc2a117a14aa
Rob Coffman <robcoffman@github.com>
2016-06-18 10:33:19 -0700

    changes to check repo script

--- scripts/check_repo.sh
+++ scripts/check_repo.sh
@@ -13,15 +13,7 @@ for repo in "${repos[@]}"
 do
   echo "Checking" ${repo}
   cd "${repo}"
-
-  status=$([ "`git log --pretty=%H ...refs/heads/master^ | head -n 1`" = "`git ls-remote origin -h refs/heads/master |cut -f1`" ] && echo "up to date" || echo "not up to date")
-
-  if [ "$status" == "up to date" ]; then
-     echo $'up to date\n'
-  else
-     echo $'not up to date\n'
-     echo $'Pulling Changes For ${repo}\n'
      git reset --hard
      git pull
-  fi
 done
+
