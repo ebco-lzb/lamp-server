1596160ed55231a2efd9c6df8b5ee1d9014060ae
Rob Coffman <rcoffman@redhat.com>
2016-06-20 10:50:50 -0700

    flush flash was deleting dir, not contents

--- scripts/flush_flash.sh
+++ scripts/flush_flash.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #monthly purge of flash Reports
-rm -rf /home/homepage/public_html/files/rank/flash/tmpdownload
-rm -rf /home/homepage.la-z-boyga.com/public_html/files/rank/flash/tmpdownload
+rm -rf /home/homepage/public_html/files/rank/flash/tmpdownload/*
+rm -rf /home/homepage.la-z-boyga.com/public_html/files/rank/flash/tmpdownload/*
 
 #Re-populate the flash reports for EBCO
 /opt/lamp-server/scripts/flash
