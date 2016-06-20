37892e303da41c32d50b670020e49cb38c3a633c
Rob Coffman <rcoffman@redhat.com>
2016-06-20 13:53:37 -0700

    moving flush to eval

--- scripts/flush_flash.sh
+++ scripts/flush_flash.sh
@@ -4,7 +4,7 @@ rm -rf /home/homepage/public_html/files/rank/flash/tmpdownload/*
 rm -rf /home/homepage.la-z-boyga.com/public_html/files/rank/flash/tmpdownload/*
 
 #Re-populate the flash reports for EBCO
-/opt/lamp-server/scripts/flash
+eval /opt/lamp-server/scripts/flash
 
 #Re-populate the flash reports for FGA
-/opt/lamp-server/scripts/flash_atl
+eval /opt/lamp-server/scripts/flash_atl
