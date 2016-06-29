df39cd13906cd7f64dc0d0eb48a5c0a0e7c4b753
Rob Coffman <robcoffman@github.com>
2016-06-29 15:20:05 -0700

    updating drush alias's

--- config/drush/aliases.drushrc.php
+++ config/drush/aliases.drushrc.php
@@ -8,3 +8,13 @@ $aliases['ebco'] = array (
   'root' => '/home/homepage/public_html',
   'uri' => 'http://homepage.la-z-boyaz.com',
 );
+
+$aliases['ebco-d8'] = array (
+  'root' => '/home/d8-ebco/public_html',
+  'uri' => 'http://d8-ebco.la-z-boyaz.com',
+);
+
+$aliases['fga-d8'] = array (
+  'root' => '/home/d8-fga/public_html',
+  'uri' => 'http://d8-fga.la-z-boyga.com',
+);
